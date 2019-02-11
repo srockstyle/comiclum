9df0abdb22825a12cd66045cd8940227e3acf8ec
srockstyle <kobayashi_shohei@srockstyle.com>
2019-02-11 18:57:40 +0900

    テストファイル自動生成の記述

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,16 @@ module Comirough
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+  # テストファイル自動生成
+  config.generators do |g|
+    g.test_framework :rspec,
+                      fixtures: true,
+                      view_specs: false,
+                      helper_specs: false,
+                      routing_specs: false,
+                      controller_specs: true,
+                      request_specs: false
+    g.fixture_replacement :factory_bot, dir: "spec/factories"
+  end
+
 end
